fa5305d9da6ed71ddf4599eec784367ff9d061aa
Andrew <a@thefestas.com>
2021-11-29 00:40:20 -0800

    fixed bug in initialize that it wasnt being properly flat

--- lbc/problems/lbc_examples/lbc_simple.py
+++ lbc/problems/lbc_examples/lbc_simple.py
@@ -1,15 +1,15 @@
 """
 3d double integrator , multi robot uncooperative target
 """
+from collections import Counter
+
 import numpy as np
 
-from lbc.problems.problem import Problem, contains
+from lbc.problems.problem import Problem
 from lbc.reward_functions import prio_reward
 
 
 def sample_vector(lims, damp=0.0):
-    # todo
-    # from cube
     dim = lims.shape[0]
     x = np.zeros((dim, 1))
     for i in range(dim):
@@ -82,7 +82,7 @@ class LbcSimple(Problem):
         self.state_lims = self.state_lims.reshape(-1, 2)
 
         self.action_lims = [
-            [0, self.num_regions]
+            [1, self.num_regions]
         ]
         self.action_lims = self.action_lims * self.num_robots
         self.action_lims = np.asarray(self.action_lims).flatten()
@@ -104,10 +104,14 @@ class LbcSimple(Problem):
         a1_goal = [9, 1]
         a1_prio = 1
 
-        region_prios = [0] * self.num_regions
         region_dists = [0] * self.num_regions
+        region_prios = [0] * self.num_regions
+        # These two lines can be used to test these entries in the state spaces as they will
+        # create distinct values rather than all 0's
+        # region_dists = np.arange(start=0, stop=self.num_regions, step=1)
+        # region_prios = np.arange(start=self.num_regions+1, stop=2*self.num_regions+1, step=1)
 
-        start_state = [
+        start_state = np.hstack((
             a0_start,
             a0_prio,
             a0_goal,
@@ -118,17 +122,15 @@ class LbcSimple(Problem):
             a1_goal,
             region_dists,
             region_prios,
-        ]
-        start_state = np.asarray(start_state)
-        start_state = start_state.flatten()
+        ))
         return start_state
 
     def reward(self, state, action):
         s_0 = state[self.state_idxs[0]]
         s_1 = state[self.state_idxs[1]]
-        r_0 = prio_reward(s_0, action, num_regions=self.num_regions)
-        r_1 = prio_reward(s_1, action, num_regions=self.num_regions)
-        rew = np.asarray[[r_0], r_1]
+        r_0 = prio_reward(s_0, action)
+        r_1 = prio_reward(s_1, action)
+        rew = np.array([[r_0], [r_1]])
         return rew
 
     def normalized_reward(self, state, action):
@@ -136,10 +138,46 @@ class LbcSimple(Problem):
 
     def step(self, s, a, dt):
         # todo
-        return
+        return self.initialize()
 
     def render(self, states=None, fig=None, ax=None):
         # todo
+        if fig is None or ax is None:
+            fig, ax = plotter.make_3d_fig()
+
+        if states is not None:
+
+            lims = self.state_lims
+            colors = plotter.get_n_colors(self.num_robots)
+            for robot in range(self.num_robots):
+                robot_state_idxs = self.state_idxs[robot]
+
+                ax.plot(states[:, robot_state_idxs[0]].squeeze(axis=1), states[:, robot_state_idxs[1]].squeeze(axis=1),
+                        states[:, robot_state_idxs[2]].squeeze(axis=1), color=colors[robot])
+                ax.plot(states[0, robot_state_idxs[0]], states[0, robot_state_idxs[1]], states[0, robot_state_idxs[2]],
+                        color=colors[robot], marker='o')
+                ax.plot(states[-1, robot_state_idxs[0]], states[-1, robot_state_idxs[1]],
+                        states[-1, robot_state_idxs[2]], color=colors[robot], marker='s')
+
+                # projections
+                ax.plot(lims[0, 0] * np.ones(states.shape[0]), states[:, robot_state_idxs[1]].squeeze(),
+                        states[:, robot_state_idxs[2]].squeeze(),
+                        color=colors[robot], linewidth=1, linestyle="--")
+                ax.plot(states[:, robot_state_idxs[0]].squeeze(), lims[1, 1] * np.ones(states.shape[0]),
+                        states[:, robot_state_idxs[2]].squeeze(),
+                        color=colors[robot], linewidth=1, linestyle="--")
+                ax.plot(states[:, robot_state_idxs[0]].squeeze(), states[:, robot_state_idxs[1]].squeeze(),
+                        lims[2, 0] * np.ones(states.shape[0]),
+                        color=colors[robot], linewidth=1, linestyle="--")
+
+            ax.set_xlim((lims[0, 0], lims[0, 1]))
+            ax.set_ylim((lims[1, 0], lims[1, 1]))
+            ax.set_zlim((lims[2, 0], lims[2, 1]))
+            ax.set_box_aspect((lims[0, 1] - lims[0, 0], lims[1, 1] - lims[1, 0], lims[2, 1] - lims[2, 0]))
+
+            for robot in range(self.num_robots):
+                ax.scatter(np.nan, np.nan, np.nan, color=colors[robot], label="Robot {}".format(robot))
+            ax.legend(loc='best')
         return fig, ax
 
     def is_terminal(self, state):
@@ -150,7 +188,7 @@ class LbcSimple(Problem):
         # todo: obstacles
         state = np.array([np.asarray(x) for x in state])
         state = np.hstack(state)
-        return ((self.state_lims[:,0] <= state).all() and (state <= self.state_lims[:,1])).all()
+        return ((self.state_lims[:, 0] <= state).all() and (state <= self.state_lims[:, 1])).all()
 
     def policy_encoding(self, state, robot):
         return state
@@ -165,3 +203,28 @@ class LbcSimple(Problem):
     def plot_value_dataset(self, dataset, title):
         # todo
         pass
+
+
+if __name__ == '__main__':
+    num_actions = 500
+    test_problem = LbcSimple()
+    action_history = []
+    for action_idx in range(0, num_actions):
+        each_action = test_problem.sample_action()
+        action_history.extend(each_action.flatten().tolist())
+    action_counts = Counter(action_history)
+    action_counts = sorted(action_counts.items())
+    action_history = np.asarray(action_history)
+    print(f'min: {np.min(action_history)} | max: {np.max(action_history)}')
+    for each_num, each_count in action_counts:
+        print(f'\t{each_num}: {each_count}')
+    ########################################
+    a0_actions = range(1, test_problem.num_regions + 1)
+    initial_reward = test_problem.normalized_reward(test_problem.initialize(), None)
+    print(f'Reward initial state: {initial_reward[0]}, {initial_reward[1]}')
+    for each_action in a0_actions:
+        full_action = np.asarray([[0], each_action])
+        initial_state = test_problem.initialize()
+        next_state = test_problem.step(initial_state, full_action, dt=1)
+        next_reward = test_problem.normalized_reward(next_state, full_action)
+        print(f'Reward of agent0 taking action {each_action}: {next_reward[0]}')
--- lbc/reward_functions.py
+++ lbc/reward_functions.py
@@ -17,29 +17,36 @@ def distance(start_coord, end_coord, degree=2):
     return dist
 
 
-def prio_reward(state, action: list, num_regions=8):
+def prio_reward(state, action: list):
     """
-    Gets the reward for a secific state-action pair.
+    Gets the reward for a specific state-action pair.
     State: [pos x, pos y, distance to goal, priority, 8 * normalized distance to closest agent, 8 * priorities]
 
-    :param state:       [vector len 21] State encoding of the agent
-    :param action:      [vector len 8] Action taken by the agent
-    :param num_regions: number of regions around the agent
+    :param state:       State encoding of the agent
+                            [0,1] location of agent
+                            [2] priority of agent
+                            [3,4] goal location
+                            Rest of the array contains information regarding the surroundings of the agent
+                            The first part is the distances to the closest agent in each region
+                            The second part if the priority of the closest agent in each region
+                            These two parts are the same (variable_ length
+    :param action:      Action taken by the agent
+                            Agent is able to take a single movement in a vector around it
     :return:            reward for this state
     """
     agent_loc = state[0:2]
+    agent_priority = state[2]
     goal_loc = state[3:5]
-    agent_priority = state[5]
 
     dist_goal = distance(start_coord=agent_loc, end_coord=goal_loc, degree=2)
     reward = 1 / dist_goal
 
     # closest agents detected in all regions around the agent
-    closest_base_idx = 6
-    priority_base_idx = closest_base_idx + num_regions + 1
-    for i in range(num_regions):
-        d_xi = state[closest_base_idx + i]
-        priority_i = state[priority_base_idx + i]
-        reward_i = (agent_priority - priority_i) * (1 - d_xi)
+    region_info = state[5:]
+    mid_idx = len(region_info)//2
+    dist_info = region_info[:mid_idx]
+    prio_info = region_info[mid_idx:]
+    for each_dist, each_prio in zip(dist_info, prio_info):
+        reward_i = (agent_priority - each_prio) * (1 - each_dist)
         reward += reward_i
     return reward
