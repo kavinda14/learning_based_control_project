59f8f6df2b76d12b3208268e1e4cfe56ef755120
Kavinda Senewiratne <senewiry@oregonstate.edu>
2021-12-05 12:04:39 -0800

    pickling and unpickling working

--- lbc/scripts/run.py
+++ lbc/scripts/run.py
@@ -11,6 +11,8 @@ from lbc.util import init_tqdm, update_tqdm, get_problem, get_solver
 import matplotlib.pyplot as plt
 import matplotlib
 
+import pickle
+
 
 def make_instance(param):
     instance = dict()
@@ -153,54 +155,19 @@ if __name__ == '__main__':
         instance = make_instance(param)
         # sim_results = [run_instance(0, Queue(), len(instance["problem"].times), instance, verbose=True)]
         sim_results = run_instance(0, Queue(), len(instance["problem"].times), instance, verbose=True)
+        
+        # pickle results
+        print("pickling started..")
+        filename = '/home/kavi/learning_based_control_project/lbc/pickles/sim_results'
+        outfile = open(filename,'wb')
+        pickle.dump(sim_results, outfile)
+        outfile.close()
+        print("pickling done!")
 
     if param.movie_on:
         print('making movie...')
         plotter.make_movie(sim_results[0], sim_results[0]["instance"], "../current/plots/vid.mp4")
         plotter.open_figs("../current/plots/vid.mp4")
-
-    # save/load results
-    # todo
-
-    # plotting
-    print('plotting results...')
-    plt.xlim(0, 11)
-    plt.ylim(0, 11)
-    matplotlib.use('TkAgg')
-    x_coords1 = []
-    y_coords1 = []
-    x_coords2 = []
-    y_coords2 = [] 
-    
-    states = sim_results["states"]
-
-    for i_state in states:
-        x_coords1.append(i_state[0])
-        y_coords1.append(i_state[1])
-        x_coords2.append(i_state[21])
-        y_coords2.append(i_state[22])   
-
-    # print(x_coords1)
-    # print(y_coords1)
-    # print(x_coords2)
-    # print(y_coords2)
-
-    print(len(states))
-    print(((len(states))-1))
-    rows = 4
-    for i in range(len(states)):
-        if (i+1 >= (len(states))-1):
-            continue
-        plt.subplot(rows, len(states)//rows, i+1)
-        plt.plot(x_coords1[0:i+1],y_coords1[0:i+1])
-        plt.plot(x_coords2[0:i+1],y_coords2[0:i+1])
-        plt.title("t{}".format(i))
-
-    plt.show() 
-
-    # plt.plot(x_coords1, y_coords1)
-    # plt.plot(x_coords2, y_coords2)
-    # plt.show()
 
     #     plotter.plot_sim_result(sim_result)
     #     sim_result["instance"]["problem"].render(states=sim_result["states"])
